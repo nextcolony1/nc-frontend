16e3024a0d34004b7f7478fe8f1b32738573b1f9
Oli <48681407+oliverschmid@users.noreply.github.com>
2020-05-13 21:25:39 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="./css/theme.css" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="64x64" href="/img/favicon.png">
+    
+    <title>404</title>
+    
+  </head>
+
+  <body>
+
+    <header id="header">
+        <?php include "include/nav_top_planet.php" ?>
+    </header>
+    <div class="container theme-showcase">
+	  
+      <div class="page-header">
+        <h2>404</h2>
+      </div>
+
+<div class="content">
+
+
+<!-- /404 -->
+
+<p>Something's gone wrong, the page you requested was not found. The requested page has been lost in the four-dimensional continuum, does not exist or has been deleted. If this page should exist, please contact the support.</p>
+
+<p>Go back to the <a href="https://nextcolony.io/"><b>Next</b>Colony homepage</a>.</p>
+
+<?php include "include/footer.php" ?>
+
+<?php include "include/modal.php" ?>
+
+        
+      </div></div> <!-- /container -->
+
+
+    <!-- Bootstrap-JavaScript
+    ================================================== -->
+    <!-- Am Ende des Dokuments platziert, damit Seiten schneller laden -->
+    <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script type ="text/javascript">
+      window.onload = function what(){
+        GetMyPlanets();
+      };
+    </script>    
+  
+</body></html>
